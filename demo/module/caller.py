cf24984872aa8ae3922c73b9e31f4c705a2ce2d2
somkiat <somkiat.p@gmail.com>
2017-07-25 10:39:59 +0700

    Example of module

---
+++ demo/module/caller.py
@@ -0,0 +1,3 @@
+from hello import *
+
+say_hi()
---
+++ demo/module/hello.py
@@ -0,0 +1,9 @@
+def say_hi():
+    print("Say hi")
+
+def _private():
+    print("Say with private")
+
+if __name__ == "__main__":
+   say_hi()
+   _private()
